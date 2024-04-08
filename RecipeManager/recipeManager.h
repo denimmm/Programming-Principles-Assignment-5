8cbbdba0e7acb257bbb4cc2e50c9edaeb2443e79
denimmm <dmacdougall7987@conestogac.on.ca>
2024-04-07 20:13:06 -0400

    created the main three structs

---
+++ RecipeManager/recipeManager.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define NAMELENGTH 30
+#define INGREDIENTLENGTH 30
+#define INSTRUCTIONLENGTH 500
+
+//all three structs are linked lists in order to make the app flexible
+typedef struct ingredient {
+	char text[INGREDIENTLENGTH];
+	struct ingredient* next;
+
+}INGREDIENT, PINGREDIENT;
+
+typedef struct instruction {
+	char text[INSTRUCTIONLENGTH];
+	struct instruction* next;
+
+}INSTRUCTION, *PINSTRUCTION;
+
+typedef struct recipe {
+	char name[NAMELENGTH];
+	PINGREDIENT Ingredients;
+	PINSTRUCTION Instructions;
+	struct recipe* next;
+
+} RECIPE, *PRECIPE;
